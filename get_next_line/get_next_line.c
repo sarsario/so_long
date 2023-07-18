48c08d40f4f143dfb66ab3da42fd1a1d832a1222
Oussama Sarsari <osarsari@student.s19.be>
2023-07-18 13:22:51 +0200

    KISS

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 12:12:44 by osarsari          #+#    #+#             */
-/*   Updated: 2023/07/18 12:19:32 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/07/18 13:22:46 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,17 @@ char	*get_next_line(int fd)
 	char		*line;
 	int			i;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0)
 		return (NULL);
 	if (!buffer)
 		buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (!buffer)
 		return (NULL);
-	
+	if (ft_strchr(buffer, '\n'))
+		return (ft_get_line(&buffer));
+	line = ft_strdup(buffer);
+	if (!line)
+		free(buffer);
+	if (!line)
+		return (NULL);
 }
