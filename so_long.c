4ee6edd2ab9fcffc9c2d174238c7abfb6ec53f74
Oussama Sarsari <osarsari@student.s19.be>
2023-08-12 16:57:49 +0200

    added images

--- so_long.c
+++ so_long.c
@@ -6,8 +6,14 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 11:55:27 by osarsari          #+#    #+#             */
-/*   Updated: 2023/08/12 11:56:25 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/08/12 12:00:34 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/so_long.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+		return ft_error("Error\nInvalid number of arguments\n");
+}
