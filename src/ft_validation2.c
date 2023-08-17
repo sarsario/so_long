09cdaed0a081e2d62ffdaf35c169d356ba94c3fb
Oussama Sarsari <osarsari@student.s19.be>
2023-08-17 17:59:35 +0200

    Recursive path finder should be ok

--- src/ft_validation2.c
+++ src/ft_validation2.c
@@ -6,7 +6,7 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 14:21:56 by osarsari          #+#    #+#             */
-/*   Updated: 2023/08/17 16:52:58 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/08/17 17:53:08 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,9 +47,24 @@ int	ft_enough_elements(char **error_msg, t_game *game)
 	return (1);
 }
 
-int	ft_check_path(char **map, int x, int y, int c)
+int	ft_check_path(char **map, int x, int y, int *c)
 {
-	
+	if (map[x][y] == 'E' || map[x][y] == 'C')
+		(*c)--;
+	if (*c == 0)
+		return (1);
+	map[x][y] = '1';
+	if (x > 0 && map[x - 1][y] != '1' && ft_check_path(map, x - 1, y, c))
+		return (1);
+	if (y > 0 && map[x][y - 1] != '1' && ft_check_path(map, x, y - 1, c))
+		return (1);
+	if (x < ft_array_len(map) - 2 && map[x + 1][y] != '1'
+		&& ft_check_path(map, x + 1, y, c))
+		return (1);
+	if (y < ft_strlen(map[x]) - 2 && map[x][y + 1] != '1'
+		&& ft_check_path(map, x, y + 1, c))
+		return (1);
+	return (0);
 }
 
 int	ft_valid_path(char **error_msg, t_game *game)
@@ -61,7 +76,7 @@ int	ft_valid_path(char **error_msg, t_game *game)
 	if (!map_cpy)
 		return (0);
 	check_point = game->collectibles + game->exit;
-	if (!ft_check_path(map_cpy, game->player_x, game->player_y, check_point))
+	if (!ft_check_path(map_cpy, game->player_x, game->player_y, &check_point))
 	{
 		*error_msg = "Error\nMap unsolvable\n";
 		ft_array_free(map_cpy);
