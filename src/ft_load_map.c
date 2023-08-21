6673a71b7423fd3c9c910659f0728bbf5cb14445
Oussama Sarsari <osarsari@student.s19.be>
2023-08-21 19:46:30 +0200

    base game done

--- src/ft_load_map.c
+++ src/ft_load_map.c
@@ -6,7 +6,7 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 12:28:28 by osarsari          #+#    #+#             */
-/*   Updated: 2023/08/21 18:26:04 by osarsari         ###   ########.fr       */
+/*   Updated: 2023/08/21 19:40:28 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,43 +71,3 @@ void	ft_draw_map(t_game game)
 	mlx_put_image_to_window(game.mlx, game.win,
 		game.img_set.player.img, game.player_x * 32, game.player_y * 32 - 8);
 }
-
-// float scale_factor = 0.01;
-// void scale_image(void *mlx_ptr, void *win_ptr, void *img_ptr, int orig_width,
-// int orig_height) {
-//     int new_width = orig_width * scale_factor;
-//     int new_height = orig_height * scale_factor;
-//     // Create a new image
-//     void *scaled_img_ptr = mlx_new_image(mlx_ptr, new_width, new_height);
-//     // Get the pixel data of the original image
-// 	int bpp_original;
-// 	int size_line_original;
-// 	int endian_original;
-//     char *orig_data = mlx_get_data_addr(img_ptr, &bpp_original,
-// &size_line_original, &endian_original); // Get original image data
-//     // Get the pixel data of the scaled image
-// 	int bpp_scaled;
-// 	int size_line_scaled;
-// 	int endian_scaled;
-//     char *scaled_data = mlx_get_data_addr(scaled_img_ptr, &bpp_scaled,
-// &size_line_scaled, &endian_scaled); // Get scaled image data
-//     // Perform nearest-neighbor scaling algorithm
-//     for (int y = 0; y < new_height; y++) {
-//         for (int x = 0; x < new_width; x++) {
-//             int orig_x = x / scale_factor;
-//             int orig_y = y / scale_factor;
-//             // Copy the pixel from the original image to the scaled image
-//             int orig_pixel_offset = (orig_y * orig_width + orig_x) * 4;
-// Assuming 32-bit RGBA format
-//             int scaled_pixel_offset = (y * new_width + x) * 4;
-//             // Copy RGBA values
-//             for (int channel = 0; channel < 4; channel++) {
-//                 scaled_data[scaled_pixel_offset + channel] =
-//	orig_data[orig_pixel_offset + channel];
-//             }
-//         }
-//     }
-//     // Display the scaled image
-//     mlx_put_image_to_window(mlx_ptr, win_ptr, scaled_img_ptr, 0, 0);
-// Display scaled image
-// }
